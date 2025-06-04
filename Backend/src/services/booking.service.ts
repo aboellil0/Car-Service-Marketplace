1c93f95610e058b0a3782284c554099f9334aae8
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-06-04 17:20:31 +0300

    Refactor booking service: update createBookingRequest interface, add BookingStatus enum, and enhance IBookingService methods for improved booking management

--- Backend/src/services/booking.service.ts
+++ Backend/src/services/booking.service.ts
@@ -9,6 +9,8 @@ import User,{IUser} from "../models/User.model"
 import Workshop,{IWorkshop} from "../models/Workshop.model"
 import Service,{IService} from "../models/Sevices.model"
 import Cars,{ ICars } from "../models/Cars.model";
+import Booking,{IBooking} from "../models/Booking.model"
+import mongoose from "mongoose";
 
 
 interface ApiResponse<T> {
@@ -18,15 +20,37 @@ interface ApiResponse<T> {
     error?: string;
 }
 
-export interface CreateBookingRequest {
+export interface createBookingRequest {
     customerId: string;
     serviceId: string;  
     workshopId: string;
     scheduledDate?: Date;
     description: string;
-  }
+}
+
+// export interface BookingCompletionRequest {
+//     finalPrice: number;
+//     workPerformed: string;
+//     partsUsed?: string[];
+//     recommendations?: string;
+// }
+
+enum BookingStatus {
+    PENDING = "pending",
+    CONFIRMED = "confirmed",
+    IN_PROGRESS = "in_progress",
+    CANCELLED = "cancelled",
+    COMPLETED = "completed"
+}
 
 export interface IBookingService{
-    createBooking(bookingData:createBookingReq) 
+    createBooking(bookingData:createBookingRequest) : Promise<ApiResponse<IBooking>>;
+    getBooking(bookingId: mongoose.Types.ObjectId) : Promise<ApiResponse<IBooking>>;
+    updateBookingStatus(bookingId: string, status: BookingStatus, providerId?: string): Promise<ApiResponse<IBooking>>;
+    cancelBooking(bookingId: string, userId: string): Promise<ApiResponse<IBooking>>;
+    getCustomerBookings(customerId: string): Promise<ApiResponse<IBooking[]>>;
+    getProviderBookings(providerId: string): Promise<ApiResponse<IBooking[]>>;
+    acceptBooking(bookingId: string, providerId: string): Promise<ApiResponse<IBooking>>;
+    rejectBooking(bookingId: string, providerId: string, reason: string): Promise<ApiResponse<IBooking>>;
 }
 
