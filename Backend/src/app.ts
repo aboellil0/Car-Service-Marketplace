ff64030b27d74957c56af7ff3edce06fb7454e0a
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-06-11 11:11:40 +0300

    feat: set up Express server with Socket.IO for real-time communication

---
+++ Backend/src/app.ts
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
---
+++ Backend/src/websoket/realtimeHnadler.ts
@@ -0,0 +1,31 @@
+import { Server } from 'socket.io';
+import { Socket } from 'socket.io';
+
+const activeconnections = new Map<string, string>();
+const WorkshopConnection = new Map<string, string>();
+
+
+io.on('connection', (socket: Socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('join', (data) => {
+        try {
+            const { userId, role} = data;
+            activeconnections.set(socket.id, {userId, role});
+            
+            if (role === 'workshop') {
+                workshopConnection.set(socket.id, userId);
+                socket.join(`workshop_${userId}`);
+                console.log(`Workshop user ${userId} connected with socket ID: ${socket.id}`);
+                
+            }else {
+                socket.join(`user_${userId}`);
+                console.log(`User ${userId} connected with socket ID: ${socket.id}`);
+            }
+            
+        }catch (error) {
+            console.error('Error in join event:', error);
+            socket.emit('error', { message: 'Failed to join', error: error.message });
+        }
+    });
+});
